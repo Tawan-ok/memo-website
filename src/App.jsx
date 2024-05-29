86f9da3bce87ed0decd7a1546c2e371d09ef8b52
Tawan-ok <104263163+Tawan-ok@users.noreply.github.com>
2024-05-29 20:56:46 +0700

    add an example

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,26 @@ function uniqueId() {
 const INITAL_TODOS = [
   {
     id: 1,
-    task: "Read a book",
-    dueDate: new Date("2023-02-28"),
+    task: "Destory Death Star",
+    dueDate: new Date("2024-01-28"),
     isFinished: false,
   },
   {
     id: 2,
-    task: "Buy dog food",
-    dueDate: new Date("2024-06-14"),
+    task: "Cleaning Schedule",
+    dueDate: new Date("2024-02-14"),
     isFinished: true,
   },
   {
     id: 3,
-    task: "Go to cinema",
-    dueDate: new Date("2023-05-20"),
+    task: "Coding",
+    dueDate: new Date("2023-03-20"),
     isFinished: true,
   },
   {
     id: 4,
-    task: "Print homework",
-    dueDate: new Date("2024-07-26"),
+    task: "Go to cinema",
+    dueDate: new Date("2024-04-26"),
     isFinished: true,
   },
 ];
